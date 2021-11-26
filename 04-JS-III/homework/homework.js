4f57203e9d00291c1d96605d98afd8aa4a5a724d
mikepipagaleano <mikebostero.bernal@gmail.com>
2021-11-26 17:51:41 -0300

    pre henry

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -26,6 +26,11 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+  var nuevoarray = [];
+  for (var i = 0; i < array.length; i++) {
+    nuevoarray[i] = array[i] + 1; 
+  }
+  return nuevoarray;
 }
 
 
